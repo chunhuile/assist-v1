b5ec4b2ed6dff0e1f8020847ce5546cee0328e90
chunhuile <Lch097022@gmail.com>
2024-05-11 02:21:48 +0800

    优化input的代码

--- src/pages/ProductManagement/index.tsx
+++ src/pages/ProductManagement/index.tsx
@@ -10,9 +10,12 @@ export default class ProductManagement extends Component {
   render() {
     return (
       <div>
+        <div style={{marginTop:"20px"}}>
+          <span style={{margin:"16px "}}>今日数据</span>
+          <ProductTable />
+        </div>
 
 
-        <ProductTable />
       </div>
     )
   }
--- src/pages/components/basic/Input.tsx
+++ src/pages/components/basic/Input.tsx
@@ -1,43 +1,50 @@
-import React ,{ChangeEvent, } from 'react';
+import React ,{ ChangeEvent } from 'react';
 import { Input } from 'antd';
 
-
-interface BasicButtonProps{
+/**
+ * @file 使用解构值，直接在函数参数中提取所需的属性值，以便使的代码更为简洁
+*/
+interface InputProps{
     handler: (e: ChangeEvent<HTMLInputElement>) => void,
     label:string,
     placeholder:string,
-    inputStyle:Object,
-    BasicInputStyle:object
+    inputStyle:React.CSSProperties;//将inputstyle属性设置为可选
+    BasicInputStyle:React.CSSProperties;//将属性值设置为可选
 }
 
 
-const BasicInput: React.FC<BasicButtonProps> = ({
+const BasicInput: React.FC<InputProps> = ({
     handler,
     label,
     placeholder,
-    inputStyle,
-    BasicInputStyle
+    inputStyle = {},//设置默认值为空对象
+    BasicInputStyle = {},//设置默认值为
 }) => {
-  
-
-
     return (
-        <div style={BasicInputStyle}>
-            {/* <span>{label}</span> */}
-            {label}
-            <Input style={inputStyle} placeholder={placeholder}
+        /**
+         * @file 将样式对象传递给组件，使用对象合并运算，将其和默认样式 进行合并，以便更加灵活
+         */ 
+        <div style={{...defaultBasicInputStyle, ...BasicInputStyle,}}>
+            <label>{label}</label>
+            <Input style={{...inputStyle,...defaultInputStyle}} placeholder={placeholder}
             onChange={handler} 
         />
-        </div>
-
-      
+        </div>      
     )
 }
 
 
-const basicInput = {
-    
-}
+// 默认样式
+const defaultBasicInputStyle: React.CSSProperties = {
+    display: "flex",
+    justifyContent: "space-between",
+    fontSize: "14px",
+    alignItems: "center"
+};
+
+const defaultInputStyle: React.CSSProperties = {
+    marginLeft: "10px"
+};
 
 
 export default BasicInput;
